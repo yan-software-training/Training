67089aed62b731c1fe10e154ff0cce26fbf4ac92
Duy14102 <bongdien77@gmail.com>
2024-12-09 14:30:03 +0700

    Hello world Typescript

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
 "use strict";
-function getCounter() {
-    let counter = function (start) {
-        console.log('start: ', start);
-    };
-    counter.interval = 123;
-    // counter.reset = function () { };
-    console.log('counter: ', counter);
-    return counter;
-}
-let c = getCounter();
-// const res = c(10);
-// console.log('c(10): ', res);
-// c.reset();
-c.interval = 5.0;
+console.log("Hello world");
